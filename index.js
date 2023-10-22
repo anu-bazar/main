eeec6decbb00e0397aa928426d25d18e8271f9f2
anu-bazar <anubazarragcaa@gmail.com>
2023-10-22 18:58:46 +0200

    updated structure, added text

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function smoothScroll(target, duration) {
     var targetElement = document.querySelector(target);
     var targetPosition = targetElement.offsetTop;
@@ -49,55 +48,3 @@ window.addEventListener('scroll', function () {
     }
 });
  
-=======
-function smoothScroll(target, duration) {
-    var targetElement = document.querySelector(target);
-    var targetPosition = targetElement.offsetTop;
-
-    var startPosition = window.pageYOffset;
-    var distance = targetPosition - startPosition;
-    var startTime = null;
-
-    function scrollAnimation(currentTime) {
-        if (startTime === null) {
-            startTime = currentTime;
-        }
-
-        var timeElapsed = currentTime - startTime;
-        var scroll = easeInOutQuad(timeElapsed, startPosition, distance, duration);
-        window.scrollTo(0, scroll);
-
-        if (timeElapsed < duration) {
-            requestAnimationFrame(scrollAnimation);
-        }
-    }
-
-    function easeInOutQuad(t, b, c, d) {
-        t /= d / 2;
-        if (t < 1) return c / 2 * t * t + b;
-        t--;
-        return -c / 2 * (t * (t - 2) - 1) + b;
-    }
-
-    requestAnimationFrame(scrollAnimation);
-}
-
-
-document.querySelectorAll('.project-button').forEach(button => {
-    button.addEventListener('click', function () {
-        smoothScroll(button.getAttribute('href'), 1000); 
-    });
-});
-
-
-window.addEventListener('scroll', function () {
-    var projectsSection = document.getElementById('projects');
-    var sectionPosition = projectsSection.getBoundingClientRect().top;
-    var screenPosition = window.innerHeight / 1.5; 
-
-    if (sectionPosition < screenPosition) {
-        projectsSection.classList.add('fade-in');
-    }
-});
- 
->>>>>>> 4cf02a956b4d347bd767d6d38ffeac758e7b84c5
